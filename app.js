ed9251b90512a1a4243649dd5b9c3702ff76548f
EC2 Default User <ec2-user@ip-172-31-11-193.ap-northeast-1.compute.internal>
2023-01-28 08:29:17 +0000

    changin github actions deployment command region

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 
-const version = '5050';
+const version = '50501';
 
 app.get('/', (req, res) => {
     // set response content    
